e9f8f444f9b8591a992711a089ab43c9f92cad77
rejuvyesh <rejuvyesh@gmail.com>
2013-02-10 15:26:54 +0530

    Shell script for creating a new post in proper format

---
+++ new_post.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+###########################################################
+# Script to create a new blog-post file in proper format  #
+# and start the editor                                    #
+###########################################################
+
+date_pattern=`date "+%Y-%m-%d"`-
+
+# Ask for Post Name
+read -r -p "Post name > "
+title=${REPLY}
+clean_title=`echo $title | tr "[:upper:]" "[:lower:]"]` #Lower Case
+clean_title=`echo $clean_title | iconv -f utf-8 -t ascii//translit` #Remove accents
+clean_title=`echo $clean_title | tr -dc '[a-z0-9\. -_]'` #Keep spaces and letters
+clean_title=`echo $clean_title | tr " " "-"` #Replace spaces by dashes
+
+filename=$date_pattern$clean_title.md
+author=`git config --get user.name`
+
+cat > "posts/"$filename <<EOF
+---
+title: $title
+author: $author
+tags:
+---
+
+EOF
+
+$EDITOR "posts/"$filename
