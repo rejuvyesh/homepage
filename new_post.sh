ceb3eb55e7d18a9f7c29cfcd2506a63d6849cefa
rejuvyesh <rejuvyesh@gmail.com>
2013-03-07 16:51:42 +0530

    Better sanitization

--- new_post.sh
+++ new_post.sh
@@ -13,6 +13,7 @@ title=${REPLY}
 clean_title=`echo $title | tr "[:upper:]" "[:lower:]"]` #Lower Case
 clean_title=`echo $clean_title | iconv -f utf-8 -t ascii//translit` #Remove accents
 clean_title=`echo $clean_title | tr -dc '[a-z0-9\. -_]'` #Keep spaces and letters
+clean_title=`echo $clean_title | tr -d '[!?@#$%\^&"']'` #Remove punctuations
 clean_title=`echo $clean_title | tr " " "-"` #Replace spaces by dashes
 
 filename=$date_pattern$clean_title.md
