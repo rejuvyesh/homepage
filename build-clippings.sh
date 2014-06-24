203c4a3509a940f0729f1332737aeaae5a68205f
rejuvyesh <mail@rejuvyesh.com>
2014-06-24 01:09:56 -0400

    Automate clipping generation

---
+++ build-clippings.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# File: build-clippings.sh
+#
+# Created: Tuesday, June 24 2014 by rejuvyesh <mail@rejuvyesh.com>
+# License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
+#
+
+cat templates/clippings.md > clippings.md
+clippings2md ~/books/reading_list/My\ Clippings.txt >> clippings.md
