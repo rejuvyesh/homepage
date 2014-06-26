e17af2489cd60f006cfa4587e443026bf227eaf2
rejuvyesh <mail@rejuvyesh.com>
2014-06-26 16:22:44 -0400

    no more needed

--- build-clippings.sh
+++ build-clippings.sh
@@ -8,5 +8,4 @@
 
 cat templates/clippings.md > clippings.md
 clippings2md ~/books/reading_list/My\ Clippings.txt >> clippings.md
-cat templates/comments.html >> clippings.md
 git add clippings.md && git commit -m "Update clippings `date`"
